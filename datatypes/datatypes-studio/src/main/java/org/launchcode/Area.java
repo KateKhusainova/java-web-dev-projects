ab35aa539848de2546a82f5cdcba4c8284c61d6b
Ekaterina <katekhusainova8@gmail.com>
2023-09-09 17:50:40 -0400

    Some changes

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter a radius:");
-        double radius = input.nextDouble();
+        //double radius = input.nextDouble();
         //double area = Circle.getArea(radius);
+        //System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        //input.close();
 
-        if (radius <= 0) {
-            System.err.println("You must enter only positive numbers");
-        //} else if (!input.hasNextDouble()) {
-            //System.err.println("You must enter only numbers");
+// Bonus
+        if (input.hasNextDouble()) {
+            double radius = input.nextDouble();
+            if (radius > 0) {
+                double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is: " + area);
+            } else {
+                System.err.println("You must enter only positive numbers");
+            }
+        } else {
+            System.err.println("You must enter only numbers");
         }
-
-
-
-        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
     }
 }
