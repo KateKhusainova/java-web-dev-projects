c954412a5679ebf9bacab8eaa1f6dd1ce9cd0ccd
Ekaterina <katekhusainova8@gmail.com>
2023-09-24 11:46:37 -0400

    Finished exercise

---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/FileEndException.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class FileEndException extends Exception {
+    public FileEndException (String message) {
+        super (message);
+    }
+}
--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,24 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         // Test out your Divide() function!
+        int x;
+        int y;
+
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Enter the point value for this student: ");
+        x = input.nextInt();
+        System.out.println("Enter the total possible point value for an assignment: ");
+        y = input.nextInt();
+        input.close();
+        Divide(x, y);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +27,48 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        for (Map.Entry<String, String> studentFile : studentFiles.entrySet()) {
+            CheckFileExtension(studentFile.getValue());
+            System.out.println("Student: " + studentFile.getKey() + " received " + CheckFileExtension(studentFile.getValue()) + " points.");
+        }
     }
+
 
     public static void Divide(int x, int y)
     {
         // Write code here!
+
+        if (y == 0) {
+           try {
+               throw new ArithmeticException("You can't divide by zero!");
+           } catch (ArithmeticException e) {
+               e.printStackTrace();
+             }
+
+        }
+        double result = x/y;
+        System.out.println(result);
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
+        int sum = 0;
+        if(fileName.contains(".java")) {
+            sum++;
+
+        } else if (fileName.equals("") || fileName.equals(null)) {
+            try {
+                throw new FileEndException ("You need to submit a file!");
+            } catch (FileEndException e) {
+                e.printStackTrace();
+            }
+        } else {
+            sum = 0;
+        }
+        return sum;
+
     }
+
 }
